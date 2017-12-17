fd7deb65ba83ff4337e7d269e982381c86ec2c3d
thevenomsnake <thevenomsnake@icloud.com>
2017-12-17 22:58:17 +0800

    Site updated: 2017-12-17 22:58:17

---
+++ js/src/waifu-tips.js.js
@@ -0,0 +1,24 @@
+function render(template, context) {
+
+    var tokenReg = /(\\)?\{([^\{\}\\]+)(\\)?\}/g;
+
+    return template.replace(tokenReg, function (word, slash1, token, slash2) {
+        if (slash1 || slash2) {  
+            return word.replace('\\', '');
+        }
+
+        var variables = token.replace(/\s/g, '').split('.');
+        var currentObject = context;
+        var i, length, variable;
+
+        for (i = 0, length = variables.length; i < length; ++i) {
+            variable = variables[i];
+            currentObject = currentObject[variable];
+            if (currentObject === undefined || currentObject === null) return '';
+        }
+        return currentObject;
+    });
+}
+String.prototype.render = function (context) {
+    return render(this, context);
+};
